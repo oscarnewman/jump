07ff407b19960eed82073944769a0c2b0d7ddf1b
Oscar Newman <newman.oscar@gmail.com>
2022-02-04 14:36:30 -0800

    Add changsets

--- packages/jump-ui/prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	arrowParens: 'always',
-	singleQuote: true,
-	semi: false,
-	tabWidth: 2,
-	useTabs: true,
-}
---
+++ packages/jump-ui/src/Input/Input.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+function Input(props: Props) {
+	return <div></div>
+}
+
+export default Input
